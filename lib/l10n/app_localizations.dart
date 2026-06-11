e690ec22fc04cf02410410585bf3b0ebb8bf6d0a
GUOHAO LIU <lennney@users.noreply.github.com>
2026-06-11 17:56:44 +0000

    fix: remove unused intl import, fix analyze warnings

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:intl/intl.dart' as intl;
 
 import 'app_localizations_en.dart';
 import 'app_localizations_zh.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'l10n/app_localizations.dart';
